65e65db09d89c2f6c9847994ec76ae20e392345c
itslorik <97434533+itslorik@users.noreply.github.com>
2022-03-11 13:39:53 +0200

    Testi

---
+++ New Unity Project/Assets/Scripts/TP.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP : MonoBehaviour
+{
+    public Transform target = null;
+    public Transform target2 = null;
+    bool bJump = false;
+    bool bJump2 = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Teleport" && bJump == false && bJump2 == false)
+        {
+            this.transform.position = target.position;
+            bJump = true;
+        }
+
+        if (other.gameObject.tag == "Teleport2" && bJump == false && bJump2 == false)
+        {
+            this.transform.position = target2.position;
+            bJump2 = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Teleport")
+        {
+            bJump2 = false;
+        }
+        if (other.gameObject.tag == "Teleport2")
+        {
+            bJump = false;
+        }
+    }
+    
+    void Update()
+    {
+
+    }
+}
